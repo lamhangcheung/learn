0fe20fba3b423d5e8939e188746072627b85dd6d
lamhangcheung <lhx830825@163.com>
2025-01-16 22:26:12 +0800

    1

--- main/main.cpp
+++ main/main.cpp
@@ -1,20 +1,7 @@
-
-#include <thread>
 #include <iostream>
- 
-thread_local int threadSpecificVar = 0; // 每个线程都有自己的 threadSpecificVar
- 
-void threadFunction(int id) {
-    threadSpecificVar = id; // 设置线程特有的变量
-    std::cout << "Thread " << id << ": threadSpecificVar = " << threadSpecificVar;
-}
- 
+using namespace std;
+
 int main() {
-    std::thread t1(threadFunction, 1);
-    std::thread t2(threadFunction, 2);
- 
-    t1.join();
-    t2.join();
- 
+    cout <<"Hello"<<endl;
     return 0;
 }
